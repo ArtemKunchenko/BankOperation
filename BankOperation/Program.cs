ded49a3d78c20470afb1ca5c54373e557ea31bbd
Артем Кунченко <kunchenko911@ukr.net>
2023-11-06 23:44:47 +0200

    added class BankAccount, TransmitionManager added static method BankOperation

--- BankOperation/Program.cs
+++ BankOperation/Program.cs
@@ -1,22 +1,68 @@
 ﻿using System;
 using System.Data.SqlTypes;
+using static BankOperation.Program.BankAccount;
 
 namespace BankOperation
 {
-    internal class Program
+    public class Program
     {
+        #region CLASS BankAccount
         public class BankAccount
         {
             public string AccountNumber { get; set; }
-            public double Balance {  get; set; }    
-            public BankAccount(string accountNumber, double balance) 
-            { 
+            public double Balance { get; set; }
+            public BankAccount(string accountNumber, double balance)
+            {
                 AccountNumber = accountNumber;
                 Balance = balance;
             }
+            public delegate void BankOperationHandler(BankAccount bankAccount, double amount, string operationType);
+            public event BankOperationHandler BankOperationEvent;
+            public void OnBankOperationEvent(double amount, string operationType)
+            {
+                BankOperationEvent?.Invoke(this, amount, operationType);
+            }
+
         }
+        #endregion
+        #region CLASS TransmitionManager
+        public class TransactionManager
+        {
+            public void Deposit(BankAccount account, double amount)
+            {
+                account.Balance += amount;
+                account.OnBankOperationEvent(amount, "Deposit");
+            }
+
+            public void Withdraw(BankAccount account, double amount)
+            {
+                if (amount <= account.Balance)
+                {
+                    account.Balance -= amount;
+                    account.OnBankOperationEvent(amount, "Withdraw");
+                }
+            }
+        }
+        #endregion
         static void Main(string[] args)
         {
+            BankAccount account1 = new BankAccount("1234-5678-9101-1121", 10000);
+            BankAccount account2 = new BankAccount("1415-1617-1819-2021", 15000);
+            TransactionManager transactionManager = new TransactionManager();
+            account1.BankOperationEvent += BankOperation;
+            account2.BankOperationEvent += BankOperation;
+
+
+
+            transactionManager.Deposit(account1, 200);
+            Console.WriteLine();
+            transactionManager.Withdraw(account2, 600);
+
+            Console.ReadKey();
+        }
+        static void BankOperation(BankAccount bankAccount, double amount, string operationType)
+        {
+            Console.WriteLine($"Account: {bankAccount.AccountNumber} \nAmount: {amount} \nOperation Type: {operationType} \nBalance: {bankAccount.Balance}");
         }
     }
 }
