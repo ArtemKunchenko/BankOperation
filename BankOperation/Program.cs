d54f1677d1c01e9c687027e5370a4870adb6280c
Артем Кунченко <kunchenko911@ukr.net>
2023-11-06 22:22:27 +0200

    Add project files.

---
+++ BankOperation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace BankOperation
+{
+    internal class Program
+    {
+        public class BankAccount
+        {
+            public string AccountNumber { get; set; }
+            public double Balance {  get; set; }    
+            public BankAccount(string accountNumber, double balance) 
+            { 
+                AccountNumber = accountNumber;
+                Balance = balance;
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
